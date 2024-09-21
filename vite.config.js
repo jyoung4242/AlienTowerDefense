fa9d0ddaeb45c17bcb2bc8aa662fe4d6efc655ce
Justin Young <justin_dean_young@yahoo.com>
2024-09-21 14:57:02 -0400

    first build

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,9 @@
-export default {
+import { defineConfig } from "vite";
+
+export default defineConfig({
   base: "./",
-};
+  build: {
+    target: "esnext", //browsers can handle the latest ES features
+  },
+  assetsInclude: ["**/*.png", "**/*.jpg", "**/*.svg", "**/*.tff"],
+});
